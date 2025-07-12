91f37dae5b61261f370a56cfdd8363ee5ac6e1a0
MiguelCiteli <miguelciteli@gmail.com>
2025-07-12 12:14:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -204,15 +204,10 @@ if menu == "🔍 Buscar por Instituto":
 
             if "caio" in st.session_state.professor_escolhido:
                 info_caio_ribeiro()
-                criar_perfil("Caio Ribeiro")
-                st.markdown("### Perfis Cadastrados")
-                carregar_perfis()
 
             elif "alexandre" in st.session_state.professor_escolhido:
                 st.info("Informações de Alexandre Dodonov ainda não disponíveis.")
-                st.markdown("### Perfis Cadastrados")
-                carregar_perfis("perfis_alexandre.json")
-
+				
             else:
                 st.warning("Professor não encontrado.")
 
