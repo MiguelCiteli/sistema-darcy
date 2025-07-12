d1cf0b431a8ccd8cf4a2dc826661c92bcda9341b
MiguelCiteli <miguelciteli@gmail.com>
2025-07-12 12:28:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -193,9 +193,9 @@ if menu == "🔍 Buscar por Instituto":
             st.session_state.nucleo_escolhido = exibir_nucleos()
 
         st.session_state.nucleo_escolhido = exibir_nucleos()
-
+		
 		if st.session_state.nucleo_escolhido and st.session_state.nucleo_escolhido != "nucleo_7":
-    		st.info("Ainda não existem professores cadastrados para este núcleo.")
+			st.info("Ainda não existem professores cadastrados para este núcleo.")
 
 		if st.session_state.nucleo_escolhido == "nucleo_7":
     		st.markdown("### Professores do núcleo de Óptica Quântica")
